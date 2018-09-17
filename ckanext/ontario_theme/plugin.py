d8a896bcbc237cc8d7107fab6627a070feb4d4dd
Sayan <Bianca.Sayan@ontario.ca>
2018-09-17 16:20:56 -0400

    Task: Place 'My Datasets' first in user dashboard menu

--- ckanext/ontario_theme/plugin.py
+++ ckanext/ontario_theme/plugin.py
@@ -204,12 +204,11 @@ class OntarioThemePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IBlueprint)
     plugins.implements(plugins.ITemplateHelpers)
 
-    # IConfigurer
-
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'ontario_theme')
+
         # Uncomment these to use bootstrap 2 theme and comment out
         # the above template and resource directories.
         # toolkit.add_template_directory(config_, 'templates-bs2')
